e211f8767d8d58fa8b474319f522286c5d5ec154
David-Harry-77 <164024712+David-Harry-77@users.noreply.github.com>
2024-09-16 11:58:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -888,4 +888,4 @@ def calculate_results(df, subjects):
     return combined_results, failure_counts
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
